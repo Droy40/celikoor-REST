e280b29bc249a9f9824a27c9698844aa6ca0b78d
Dustin Delroy <dustindelroy@gmail.com>
2024-12-25 23:28:08 +0700

    Update ActorRepository.java

--- src/main/java/com/example/celikoor_RESTful/repository/ActorRepository.java
+++ src/main/java/com/example/celikoor_RESTful/repository/ActorRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="actor")
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
     @Modifying
     @Query("update actors a set a.name = :name where a.id = :id")
